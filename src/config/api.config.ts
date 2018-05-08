7e5d5952ffdbd5b401d8955f623735c2f84ef1cc
Jeterson Miranda <jetersonsi@gmail.com>
2018-05-08 14:51:44 -0400

    mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'http://localhost:8080',
+    bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-jet'
+
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -16,6 +18,8 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  items:CategoriaDTO[];
+  bucketUrl:string = API_CONFIG.bucketBaseUrl;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -25,7 +29,7 @@ export class CategoriasPage {
 
   ionViewDidLoad() {
     this.categoriaService.findAll().subscribe(response => {
-      console.log(response);
+      this.items = response;
     }, 
     error=> {
       console.log(error);
