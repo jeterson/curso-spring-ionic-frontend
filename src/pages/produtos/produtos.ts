7fb02788446f49f35bc33e5d8dc5d3e7e117e659
Jeterson Miranda <jetersonsi@gmail.com>
2018-05-10 09:50:26 -0400

    carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/protuto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 
 
@@ -20,7 +21,17 @@ export class ProdutosPage {
   let categoria_id = this.navParams.get('categoria_id');
     this.produtoService.findByCategoria(categoria_id).subscribe(res=>{      
       this.items = res['content'];
+      this.loadImageUrl();
     }, error=>{});
+  }
+
+  loadImageUrl(){
+    for(let p of this.items){
+      this.produtoService.getSmallImageFromBucket(p.id).subscribe(res=>{
+        p.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${p.id}-small.jpg`;
+      }, error=>{        
+      });
+    }
   }
 
   
