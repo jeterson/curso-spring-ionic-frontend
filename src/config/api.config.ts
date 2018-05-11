c484dbf105a946a854fb5c1d279eeb61e18f1727
Jeterson Miranda <jetersonsi@gmail.com>
2018-05-11 10:45:47 -0400

    Tesntando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'https://curso-spring-ionic-jet.herokuapp.com',
     bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-jet'
 
 }
